d603b8734f16251ad4a6ed3b1fc90f67909c3064
DesireeBesnard <desiree.besnard@gmail.com>
2019-12-04 15:32:20 +0000

    etape 1 et moitié etape 2

---
+++ js/script.js
@@ -0,0 +1,99 @@
+function function1() {
+  var checkBox = document.getElementById("check1");
+  var text = document.getElementById("text1");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function2() {
+  var checkBox = document.getElementById("check2");
+  var text = document.getElementById("text2");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function3() {
+  var checkBox = document.getElementById("check3");
+  var text = document.getElementById("text3");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function4() {
+  var checkBox = document.getElementById("check4");
+  var text = document.getElementById("text4");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function5() {
+  var checkBox = document.getElementById("check5");
+  var text = document.getElementById("text5");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function6() {
+  var checkBox = document.getElementById("check6");
+  var text = document.getElementById("text6");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function7() {
+  var checkBox = document.getElementById("check7");
+  var text = document.getElementById("text7");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function8() {
+  var checkBox = document.getElementById("check8");
+  var text = document.getElementById("text8");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function9() {
+  var checkBox = document.getElementById("check9");
+  var text = document.getElementById("text9");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
+
+function function10() {
+  var checkBox = document.getElementById("check10");
+  var text = document.getElementById("text10");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
